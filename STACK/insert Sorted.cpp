9adb47e83e363b653cca663a79f868c0179d20e5
Ritesh Solke <riteshsolke12@gmail.com>
2025-10-03 00:40:59 +0530

    Create insert Sorted.cpp

---
+++ STACK/insert Sorted.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std ;
+
+void sortedinsert(stack<int>& s , int value){
+
+    if(s.empty()){
+        s.push(value) ;
+        return ;
+    }
+
+    if(value >= s.top()){
+        s.push(value) ;
+        return ;
+    }
+    else{
+        int topvalue = s.top() ;
+        s.pop() ;
+
+        sortedinsert(s,value) ;
+        s.push(topvalue) ;
+    }    
+
+}
+
+int main(){
+
+    stack<int> s ;
+
+    s.push(10) ;
+    s.push(20) ;
+    s.push(30) ;
+    s.push(40) ;
+    s.push(50) ; 
+
+    sortedinsert(s,25) ;
+    cout << "Printing the Stack" << endl ;
+    while(!s.empty()){
+        cout << s.top() << " " ;
+        s.pop() ;
+    }
+    cout << endl ;
+
+
+    return 0;
+
+}
