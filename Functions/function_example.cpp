b5a335551b9ac35b9f9cf76d0614af52d29a9ea1
Ritesh Solke <riteshsolke12@gmail.com>
2025-12-26 12:26:57 +0530

    Create function_example.cpp

---
+++ Functions/function_example.cpp
@@ -0,0 +1,27 @@
+✅ Example 1: Function with Arguments and Return Value (Most Used)
+Program: Add two numbers using function
+#include <iostream>
+using namespace std;
+
+// Function declaration
+int add(int a, int b);
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    int result = add(x, y);
+    cout << "Sum = " << result;
+
+    return 0;
+}
+
+// Function definition
+int add(int a, int b) {
+    return a + b;
+}
+
+Output:
+Enter two numbers: 5 3
+Sum = 8
