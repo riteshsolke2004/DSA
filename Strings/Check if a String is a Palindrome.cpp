805f46dd59287fb31e4f7fa02ee9620bbb03a54f
Ritesh Solke <riteshsolke12@gmail.com>
2026-03-18 09:40:52 +0530

    Update Check if a String is a Palindrome.cpp

--- Strings/Check if a String is a Palindrome.cpp
+++ Strings/Check if a String is a Palindrome.cpp
@@ -2,7 +2,7 @@ Check if a String is a Palindrome
 
 Given a string s, your task is to determine whether the string is a palindrome or not. A string is considered 
 a palindrome if it reads the same forwards and backwards.
-
+ 
 For instance, the string "racecar" is a palindrome because it reads the same from left to right and right to left. 
 Conversely, the string "hello" is not a palindrome due to the difference in the sequence when read in reverse.
 
